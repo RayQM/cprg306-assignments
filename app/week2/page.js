2c5b8bc8396d5caef32548059c93e1a3754764d2
Qiaomu Lei <qiaomulei@gmail.com>
2023-09-21 15:23:46 -0600

    week3

--- app/week2/page.js
+++ app/week2/page.js
@@ -1,7 +1,7 @@
 import StudentInfo from "../StudentInfo"
 export default function ShoppingList(){
     return(
-        <main class="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen items-center justify-between p-24 display:flex;">
             <h1 class='text-6xl  w-full'>My Shopping List</h1>
             <StudentInfo/>
             <a href="/">back home</a>
---
+++ app/week3/item.js
@@ -0,0 +1,9 @@
+export default function Item(Item){
+    let text = `Buy ${Item.quantity} in ${item.category}`;
+    <ul>
+        <il><h1>{Item.name}</h1></il>
+        <il>{text}</il>
+    </ul>;
+   
+    
+}
