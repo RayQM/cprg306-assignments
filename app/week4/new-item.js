20a99dda9beafbf4ca240b1ca5fc7ee51e2a649f
Qiaomu Lei <115581793+RayQM@users.noreply.github.com>
2023-10-01 19:07:43 -0600

    week4
    not finish yet

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -4,19 +4,19 @@ import { useState } from "react"
 
 export function NewItem(){
    
-    const {name, setName} = useState("")
-    const {quantity, setQuantity} = useState(1)
-    const {category, setCategory} = useState("Produce")
+    const [name, setName] = useState("")
+    const [quantity, setQuantity] = useState(1)
+    const [category, setCategory] = useState("Produce")
     
 
     const handleSubmit=(event)=>{
         event.preventDefault()
-        var item = {name, quantity, category}
+        var item = [name, quantity, category]
         console.log (item)
         alert(`you add ${quantity} of ${name} to ${category}`)
         setName("")
         setQuantity(1)
-        setCategory(produce)
+        setCategory("Produce")
     }
     const categories = [
         "Produce",
@@ -31,11 +31,21 @@ export function NewItem(){
         "Household",
         "Other",
       ];
+      const handleNameChange=(event)=>{
+        setName(event.target.value)
+      }
+      const handleQuantityChange=(event)=>{
+        setQuantity(event.target.value)
+      }
+      const handleCategoryChange=(event)=>{
+        setCategory(event.target.value)
+      }
+
     return(
     <form onSubmit={handleSubmit}>
-        <input value={name} onChange={setName} placeholder="Enter item name plz" required/>
-        <input type="number" min="1" max="99" value={quantity} onChange={setQuantity} required/>
-        <select value={category} onChange={setCategory}> 
+        <input value={name} onChange={handleNameChange} placeholder="Enter item name plz" required className="w-40 "/>
+        <input type="number" min="1" max="99" value={quantity} onChange={handleQuantityChange} required className="text-black "/>
+        <select value={category} onChange={handleCategoryChange} className=" text-black round " > 
         {categories.map((category) => (
           <option>
             {category}
