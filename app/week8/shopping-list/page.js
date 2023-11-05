531c9d108103c2cdefa34c2e680b544a5a323100
Qiaomu Lei <115581793+RayQM@users.noreply.github.com>
2023-11-04 20:32:00 -0600

    week 8

---
+++ app/week8/shopping-list/page.js
@@ -0,0 +1,29 @@
+"use client"
+import {NewItem}  from "./new-item";
+import ItemList from "./item-list";
+import itemsData from "./items.json"
+import { useState } from "react";
+import { MealIdeas } from "./meal-ideas";
+
+export default function Page(){
+    //Initialize a state variable 
+    const[items,setItems] = useState(itemsData);
+    const[selectedItemName, setSelectedItemName] = useState(null)
+    //Create an event handler function 
+    function handleAddItem(newItem){
+        console.log(newItem);
+        setItems((currentItems) => [...currentItems, newItem]);
+    }
+    function handleItemSelect(ingredient){
+        let cleanedStr = ingredient.replace(/[^a-z\s]+$/i, ' ').trim().split(',')[0].replace(' ', '_');
+        setSelectedItemName(cleanedStr); 
+    }
+    return(
+        <main>
+            <h1>Shopping list</h1>
+            <NewItem onAddItem={handleAddItem}/>
+            <ItemList Items={items} onItemSelect={handleItemSelect}/>
+            <MealIdeas ingredient={selectedItemName}/>
+        </main>
+    )
+}
